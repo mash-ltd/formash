c69b97eb3065f95a9e0a89dadcf7140cee6a75bc
ahmadsherif <ahmad.sherif@mashsolvents.com>
2012-10-09 19:02:27 +0200

    Making ApplicationController inherit directly from app's ApplicationController

--- app/controllers/for_mash/application_controller.rb
+++ app/controllers/for_mash/application_controller.rb
@@ -1,23 +1,2 @@
-module ForMash
-  class ApplicationController < ActionController::Base
-    unless method_defined? :current_entity
-      def current_entity
-        raise '`current_entity` method has to be overriden to return an object representing an app user.'
-      end
-    end
-    helper_method :current_entity
-
-    unless method_defined? :current_user
-      def current_user
-        raise '`current_user` method has to be overriden to return the current user'
-      end
-    end
-    helper_method :current_user
-
-    unless method_defined? :authenticate_entity!
-      def authenticate_entity!
-        raise '`authenticate_entity!` method has to be overriden to return whether the current entity is authenticated or not.'
-      end
-    end
-  end
+class ForMash::ApplicationController < ApplicationController
 end
