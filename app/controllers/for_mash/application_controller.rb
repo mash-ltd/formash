be3a8982dc8cfc0d862f1c93988d6a7f1b6491f0
ahmadsherif <ahmad.sherif@mashsolvents.com>
2012-10-09 16:52:45 +0200

    Removing a method call was left by mistake

--- app/controllers/for_mash/application_controller.rb
+++ app/controllers/for_mash/application_controller.rb
@@ -2,7 +2,6 @@ module ForMash
   class ApplicationController < ActionController::Base
     unless method_defined? :current_entity
       def current_entity
-        current_user
         raise '`current_entity` method has to be overriden to return an object representing an app user.'
       end
     end
